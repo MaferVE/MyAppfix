d8d9de3226d667e37176ab977a27492705530033
MaferVE <mafervemg@outlook.com>
2018-07-14 15:19:56 -0400

    EFCore

---
+++ Data/DbAccess/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using Enti;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.DbAccess
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
---
+++ Enti/Movie.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Enti
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
